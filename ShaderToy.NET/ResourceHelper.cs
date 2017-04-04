2f5640133daf27c9c2a43423aa11e30c75b5df29
Armin Kazemi <arminkz@live.com>
2017-04-04 11:07:04 +0430

    Initial Commit
    Simple Functionality with SharpGL

---
+++ ShaderToy.NET/ResourceHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ShaderToy.NET
+{
+    static class ResourceHelper
+    {
+        public static string LoadTextFromRecource(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            //resourceName = "MyCompany.MyProduct.MyFile.txt";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
